06725ea098f6cfd0e46f172cc6771f9c007b6fcb
likeacube <indispost@gmail.com>
2015-10-19 19:07:06 -0300

    CargaMasiva con conexion BD- devolver datos en excel

--- EncuentraloFacil/EncuentraloFacil/src/java/cl/encuentraloFacil/aplicacion/Controlador/BeanCargarLeerDatos.java
+++ EncuentraloFacil/EncuentraloFacil/src/java/cl/encuentraloFacil/aplicacion/Controlador/BeanCargarLeerDatos.java
@@ -4,6 +4,7 @@
  * and open the template in the editor.
  */
 package cl.encuentraloFacil.aplicacion.Controlador;
+
 import cl.encuentraloFacil.aplicacion.Business.CargaArchivoBusiness;
 import java.util.ArrayList;
 import java.util.Iterator;
@@ -11,6 +12,9 @@ import java.util.List;
 import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.context.FacesContext;
+import javax.servlet.http.HttpServletResponse;
+import org.apache.poi.hssf.usermodel.HSSFCell;
+import org.apache.poi.hssf.usermodel.HSSFRow;
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 import org.apache.poi.hssf.usermodel.HSSFSheet;
 import org.apache.poi.poifs.filesystem.POIFSFileSystem;
@@ -19,80 +23,139 @@ import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.Row;
 import org.primefaces.model.UploadedFile;
 import org.primefaces.event.FileUploadEvent;
+
 /**
  *
  * @author FelipeSilva
  */
 @ManagedBean
 public class BeanCargarLeerDatos {
-    
+
     private UploadedFile file;
     private CargaArchivoBusiness archivoBusiness;
-    
-    public void cargarLeerDatos(FileUploadEvent event){
-        
+
+    public void cargarLeerDatos(FileUploadEvent event) {
+
         try {
-         
-            if(event.getFile()!=null)  {
-             file = event.getFile();
-            //Get the workbook instance for XLS file 
-            FacesMessage message = new FacesMessage("Succesful", file.getFileName() + " is uploaded.");
-            FacesContext.getCurrentInstance().addMessage(null, message);
-            HSSFWorkbook workbook = new HSSFWorkbook(file.getInputstream());
 
-            //Get first sheet from the workbook
-            HSSFSheet sheet = workbook.getSheetAt(0);
-            
-            //Iterate through each rows from first sheet
-            Iterator<Row> rowIterator = sheet.iterator();
-            
-            List<Integer> prod = new ArrayList<Integer>();
-            List<Integer> prodRep = new ArrayList<Integer>();
-            while (rowIterator.hasNext()) {
-                Row row = rowIterator.next();
-                //For each row, iterate through each columns
-                Iterator<Cell> cellIterator = row.cellIterator();
-                while (cellIterator.hasNext()) {
+            if (event.getFile() != null) {
+                file = event.getFile();
+                //Get the workbook instance for XLS file 
+                FacesMessage message = new FacesMessage("Succesful", file.getFileName() + " is uploaded.");
+                FacesContext.getCurrentInstance().addMessage(null, message);
+                HSSFWorkbook workbook = new HSSFWorkbook(file.getInputstream());
 
-                    Cell cell = cellIterator.next();
+                //Get first sheet from the workbook
+                HSSFSheet sheet = workbook.getSheetAt(0);
 
-                    switch (cell.getCellType()) {
-                        case Cell.CELL_TYPE_BOOLEAN:
-                            System.out.print(cell.getBooleanCellValue() + "\t\t");
-                            break;
-                        case Cell.CELL_TYPE_NUMERIC:
-                            System.out.print(cell.getNumericCellValue() + "\t\t");
-                            Integer prod_temp = (int)cell.getNumericCellValue();
-                            Integer value = null;
-                            Boolean es = true;
-                            for(int x = 0; x < prod.size(); x++){
-                                 value = prod.get(x);
-                                 if (prod_temp.equals(value)) {
-                                    es = false;
-                                    prodRep.add(value);
+                //Iterate through each rows from first sheet
+                Iterator<Row> rowIterator = sheet.iterator();
+
+                List<Integer> prod = new ArrayList<Integer>();
+                List<Integer> prodRep = new ArrayList<Integer>();
+                while (rowIterator.hasNext()) {
+                    Row row = rowIterator.next();
+                    //For each row, iterate through each columns
+                    Iterator<Cell> cellIterator = row.cellIterator();
+                    while (cellIterator.hasNext()) {
+
+                        Cell cell = cellIterator.next();
+
+                        switch (cell.getCellType()) {
+                            case Cell.CELL_TYPE_BOOLEAN:
+                                System.out.print(cell.getBooleanCellValue() + "\t\t");
+                                break;
+                            case Cell.CELL_TYPE_NUMERIC:
+                                System.out.print(cell.getNumericCellValue() + "\t\t");
+                                Integer prod_temp = (int) cell.getNumericCellValue();
+                                Integer value = null;
+                                Boolean es = true;
+                                for (int x = 0; x < prod.size(); x++) {
+                                    value = prod.get(x);
+                                    if (prod_temp.equals(value)) {
+                                        es = false;
+                                        prodRep.add(value);
+                                        break;
+                                    }
+                                }
+                                if (es) {
+                                    prod.add(prod_temp);
                                     break;
-                                 } 
-                            }
-                            if (es) {
-                                     prod.add(prod_temp);
-                                     break;
-                             } 
-                            break;
-                        case Cell.CELL_TYPE_STRING:
-                            System.out.print(cell.getStringCellValue() + "\t\t");
-                            break;
+                                }
+                                break;
+                            case Cell.CELL_TYPE_STRING:
+                                System.out.print(cell.getStringCellValue() + "\t\t");
+                                break;
+                        }
                     }
                 }
-            }
-            archivoBusiness = new CargaArchivoBusiness();
-            archivoBusiness.retornaNomProd(prod);
-            } }catch(Exception e)
-        {
-           e.printStackTrace();
-        }
-                
+                archivoBusiness = new CargaArchivoBusiness();
+                if (archivoBusiness.retornaNomProd(prod) != null) {
+                    generarExcel(archivoBusiness.retornaNomProd(prod));
+                } else {
 
-}
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public void generarExcel(List<Integer> rep) {
+        byte[] xls = new byte[0];
+
+        //Libro Excel
+        HSSFWorkbook libro = new HSSFWorkbook();
+
+        //Hoja Excel
+        HSSFSheet hoja = libro.createSheet();
+
+        // Se crea una fila dentro de la hoja
+        HSSFRow fila = null;
+
+        try {
+            //cramos la celda
+            HSSFCell celda = null;
+
+            //Realizamos un ciclo para recorrer los arrelgos privados
+            for (int i = 0; i < rep.size(); i++) {
+                fila = hoja.createRow(i);
+
+                //Nombre
+                celda = fila.createCell(0);
+                celda.setCellValue(rep.get(i));
+            }
+
+            libro.setSheetName(0, "Productos Repetidos");
+
+            //Obtenemos los bytes del Excel
+            xls = libro.getBytes();
+
+            //Generamos el Excel
+            String contentType = "application/vnd.ms-excel";
+
+            //Obtenemos el Contexto
+            FacesContext fc = FacesContext.getCurrentInstance();
+
+            //Obtenemos el Response
+            HttpServletResponse response = (HttpServletResponse) fc.getExternalContext().getResponse();
+
+            response.setHeader("Content-disposition", "attachment; filename=" + "ProductosRepetidos");
+
+            //Se indicamos el tipo de contenido
+            response.setContentType(contentType);
+
+            //Escribimos en el Response los bytes
+            response.getOutputStream().write(xls); //Forzamos a la finalización y cierre de los objetos necesarios
+            response.getOutputStream().flush();
+            response.flushBuffer();
+            fc.responseComplete();
+
+        } catch (Exception e) {
+            System.out.println("Error generando Excel.");
+        }
+    }
 
     /**
      * @return the file
