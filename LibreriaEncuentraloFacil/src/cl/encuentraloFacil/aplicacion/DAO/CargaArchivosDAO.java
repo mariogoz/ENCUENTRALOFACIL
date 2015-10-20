cb8955825b0be6e84526ed797b7230fa54b24c37
likeacube <indispost@gmail.com>
2015-10-20 00:51:49 -0300

    Mejoras en CargaMasiva con conexion BD- devolver datos en excel

--- LibreriaEncuentraloFacil/src/cl/encuentraloFacil/aplicacion/DAO/CargaArchivosDAO.java
+++ LibreriaEncuentraloFacil/src/cl/encuentraloFacil/aplicacion/DAO/CargaArchivosDAO.java
@@ -28,53 +28,45 @@ public class CargaArchivosDAO {
     public List<Integer> cargarDatos(List<Integer> prod) {
         List<Integer> productos = new ArrayList<>();
         List<String> querys = new ArrayList<>();
-        productos = prod;
-        List<Integer> repetidos =  new ArrayList<>();
-                
+        List<Integer> repetidos = new ArrayList<>();
+        int sizeRs = 0;
         int contador = 0;
         try {
+            //Es necesario tomar la variable id empresa para pasarla como parametro
+            cst = conn.getConnection().prepareStatement("select * from producto_empresa where empresa_idemp = 15");
+            ResultSet rs = cst.executeQuery();
+            rs.last();
+            sizeRs = rs.getRow();
+            for (int x = 0; x < prod.size(); x++) {
+                rs.beforeFirst();
+                int cont = 0;
+                while (rs.next()) {
+                    int prodbd = rs.getInt(1);
+                    cont++;
+                    if (prodbd == prod.get(x)) {
+                        repetidos.add(x);
+                        System.out.println("Repetido " + prodbd);
+                        cont = 0;
+                    }
+                    if (sizeRs == cont) {
+                        productos.add(prod.get(x));
+                        System.out.println("Nuevo " + prod.get(x));
+                    }
+                }
+            }
+
             PreparedStatement ps = conn.getConnection().prepareStatement("insert into producto_empresa values(?,?);");
-            for (Integer prod1 : prod) {
+            for (Integer prod1 : productos) {
                 ps.setInt(1, Integer.parseInt(prod1.toString()));
                 //Es necesario tomar la variable id empresa para pasarla como parametro
                 ps.setInt(2, 15);
                 ps.addBatch();
                 contador++;
-                if (contador >= 100 || contador == prod.size()) {
-                    int[] numUpdates = ps.executeBatch();
-                    for (int i = 0; i < numUpdates.length; i++) {
-                        if (numUpdates[i] == SUCCESS_NO_INFO) {
-                            System.out.println("Execution " + i
-                                    + ": unknown number of rows updated");
-                        } else {
-                            System.out.println("Carga " + i + 1 + "exitosa: " + numUpdates[i] + " rows updated");
-                        }
-                        contador = 0;
-                    }
-                }
-
+                ps.executeBatch();
             }
         } catch (SQLException e) {
             e.printStackTrace();
-            try {
-                cst = conn.getConnection().prepareStatement("select * from producto_empresa where empresa_idemp = 15");
-                ResultSet rs =  cst.executeQuery();
-                while(rs.next())
-                {
-                    int prodbd = rs.getInt(1);
-                    for(int x = 0; x < prod.size(); x++)
-                    {
-                        if(prodbd == prod.get(x)){
-                            repetidos.add(x);
-                            System.out.println("Número ID_PRODUCTO "+prodbd+" repetido.");
-                        }
-                    }
-                }
-            } catch (SQLException ex) {
-                Logger.getLogger(CargaArchivosDAO.class.getName()).log(Level.SEVERE, null, ex);
-            }
         }
         return repetidos;
     }
-    
 }
